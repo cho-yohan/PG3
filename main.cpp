58f6cd9deda9f7b24da00b2455554e40a4cd5211
cho-yohan <k023g1037@g.neec.ac.jp>
2024-06-21 12:40:14 +0900

    04_03_確認課題

--- main.cpp
+++ main.cpp
@@ -1,1 +1,38 @@
+﻿#include <stdio.h>
 
+class fpsGame
+{
+public:
+	virtual void Voice() {
+		printf("あにまる¥n");
+	};
+};
+
+class Valorant : public fpsGame
+{
+public:
+	void Voice() {
+		printf("Valorantっす\n");
+	};
+};
+
+class Apex : public fpsGame
+{
+public:
+	void Voice() {
+		printf("Apexっす\n");
+	};
+};
+
+int main(void) {
+
+	fpsGame* animal[2] = { new Valorant,new Apex };
+
+	animal[0]->Voice();
+	animal[1]->Voice();
+
+	delete animal[0];
+	delete animal[1];
+
+	return 0;
+}
